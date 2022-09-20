abb4916fdbe20af374e8a1497dfe4cfe78a95d52
snacccc <80086013+snacccc@users.noreply.github.com>
2022-09-20 13:16:31 -0400

    added hella stuff

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let key1= "https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/inception"
+let key2= "https://imdb-api.com/en/API/Posters/k_jtrut4iw/tt1375666"
+
+const searchBox = document.getElementById("searchBox")
