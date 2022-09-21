a2a2ac8e8004a40384dfaeef987200cc6d70e6c7
snacccc <evanlg741@gmail.com>
2022-09-21 07:53:13 -0400

    yes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-let key1= "https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/inception"
-let key2= "https://imdb-api.com/en/API/Posters/k_jtrut4iw/tt1375666"
+let key1= "https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/inception";
+let key2= "https://imdb-api.com/en/API/Posters/k_jtrut4iw/tt1375666";
 
-const searchBox = document.getElementById("searchBox")
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
+console.log(key1);
+console.log(key2);
+
+function getData(e) {
+    let input = searchInput.textContent;
+    console.log(input);
+}
+
+searchInput.addEventListener('input', getData);
+searchBtn.addEventListener('click', getData);
