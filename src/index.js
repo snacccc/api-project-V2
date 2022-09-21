f46fb0493219e5e97e97bce07418f05b213955b6
snacccc <80086013+snacccc@users.noreply.github.com>
2022-09-21 13:16:27 -0400

    added fetch and basic display function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-let key1= "https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/inception";
-let key2= "https://imdb-api.com/en/API/Posters/k_jtrut4iw/tt1375666";
+let key= "https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/inception";
 
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
 
-console.log(key1);
-console.log(key2);
+console.log(key);
 
-function getData(e) {
-    let input = searchInput.textContent;
+function getData() {
+    searchInput.input;    
+    let input = searchInput.value;
     console.log(input);
+    let key = `https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/${input}`;
+    console.log(key);
+    getInfo(key);
 }
 
-searchInput.addEventListener('input', getData);
-searchBtn.addEventListener('click', getData);
+const getInfo = async function (key) {
+    try {
+
+        const response = await fetch(key);
+        const data = await response.json();
+        console.log(data);   
+
+        const movieID = data.results[0]['id'];
+        const movieTitle = data.results[0]['title'];
+        const moviePoster = data.results[0]['title'];
+        console.log(movieID);
+        console.log(movieTitle);
+        console.log(moviePoster);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
