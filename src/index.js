48e69de2dfb652173649381009c1d6732a6471ae
snacccc <80086013+snacccc@users.noreply.github.com>
2022-09-22 13:14:13 -0400

    finished js, added a bit of scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ let key= "https://imdb-api.com/en/API/SearchMovie/k_jtrut4iw/inception";
 
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
+const movieName = document.getElementById("movieName");
+const moviePoster = document.getElementById("moviePoster");
+const movieError = document.getElementById("movieError");
 
 console.log(key);
 
@@ -17,18 +20,32 @@ function getData() {
 const getInfo = async function (key) {
     try {
 
+        movieError.innerText = "";
+        movieError.style.opacity = 0;
+
         const response = await fetch(key);
         const data = await response.json();
         console.log(data);   
 
-        const movieID = data.results[0]['id'];
+        if (!data.results[0]) {
+            movieError.innerText = "Unable to find specified movie";
+            movieError.style.opacity = 1;
+            return;
+        } else {
+
         const movieTitle = data.results[0]['title'];
-        const moviePoster = data.results[0]['title'];
-        console.log(movieID);
+        const movieImage = data.results[0]['image'];
         console.log(movieTitle);
         console.log(moviePoster);
+        movieName.innerHTML = movieTitle;
+        moviePoster.src = movieImage;
+
+        }
 
     } catch (error) {
         console.log(error);
+        movieError.innerText = "Something went wrong";
+        movieError.style.opacity = 1;
+        return;
     }
 }
